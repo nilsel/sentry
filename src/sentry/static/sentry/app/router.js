03eada929362abd1c2bc2176308c6a195450d530
David Cramer <dcramer@gmail.com>
2014-02-04 22:34:28 -0800

    Add slugs to urls

--- src/sentry/static/sentry/app/router.js
+++ src/sentry/static/sentry/app/router.js
@@ -5,28 +5,29 @@ define(["ember"], function(Ember){
         this.route('index', {path: '/'});
 
         this.resource('account', function(){
-            this.resource('account.settings', { path: '/settings' }, function(){
+            this.resource('account.settings', { path: '/settings/' }, function(){
                 this.route('appearance');
                 this.route('notifications');
                 this.route('identities');
             });
         });
 
-        this.resource('team', function(){
-            this.resource('team.project', { path: '/project' }, function(){
+        this.resource('team', { path: '/:team_slug/' }, function(){
+            this.resource('team.project', { path: '/:project_slug/' }, function(){
+                this.route('index', { path: '/' });
                 this.route('group');
-                this.resource('team.project.explore', { path: '/explore'}, function(){
+                this.resource('team.project.explore', { path: '/explore/' }, function(){
                     this.route('item');
                 });
-                this.route('settings');
             });
+            this.resource('team.settings', { path: '/settings/' }, function(){
+                this.route('projects');
+                this.route('members');
+                this.route('access-groups');
+            });
+
         });
 
-        this.resource('team.settings', { path: '/settings' }, function(){
-            this.route('projects');
-            this.route('members');
-            this.route('access-groups');
-        });
     });
 
     return Router;
--- src/sentry/static/sentry/app/routes/index.js
+++ src/sentry/static/sentry/app/routes/index.js
@@ -1,7 +1,7 @@
 define(["ember"], function(Ember) {
     return Ember.Route.extend({
         beforeModel: function() {
-            this.transitionTo('team.project.index');
+            this.transitionTo('team.project.index', 'team-slug', 'project-slug');
         }
     });
 });
