1e3662132aebee63c8163a584c0a1323b056e0c9
David Cramer <dcramer@gmail.com>
2014-02-23 16:32:55 -0800

    Remove high verbosity

--- src/sentry/static/sentry/app/main.js
+++ src/sentry/static/sentry/app/main.js
@@ -6,11 +6,11 @@
         require(["App", "ember"], function(App, Ember){
             var app_name = config.app_name || "App";
             root[app_name] = App = Ember.Application.create(App, {
-                LOG_ACTIVE_GENERATION: true,
-                LOG_MODULE_RESOLVER: true,
-                LOG_TRANSITIONS: true,
-                LOG_TRANSITIONS_INTERNAL: true,
-                LOG_VIEW_LOOKUPS: true
+                // LOG_ACTIVE_GENERATION: true,
+                // LOG_MODULE_RESOLVER: true,
+                // LOG_TRANSITIONS: true,
+                // LOG_TRANSITIONS_INTERNAL: true,
+                // LOG_VIEW_LOOKUPS: true
             });
             window.App = App;
         });
