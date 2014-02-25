64b1c2c0ea43d211a65a9a50dd0f3afe7c9aa3dc
David Cramer <dcramer@gmail.com>
2014-02-24 19:53:27 -0800

    Add actions to fixture

--- src/sentry/static/sentry/app/models/group.js
+++ src/sentry/static/sentry/app/models/group.js
@@ -24,6 +24,77 @@ define(["ember-data"], function(DS){
             count: randomNumber(1, 10000),
             firstSeen: new Date(),
             lastSeen: new Date(),
+            // actions are only available via details
+            actions: [
+              {
+                "type": "http_request",
+                "timestamp": "2008-09-08T22:47:31Z",
+                "url": "http://localhost/",
+                "method": "GET"
+              },
+              {
+                "timestamp": "2008-09-08T22:47:31Z",
+                "label": "Arbitrary event",
+                "message": "Somethin' happened bro"
+              },
+              {
+                "type": "exception",
+                "timestamp": "2008-09-08T22:47:31Z",
+                "values": [
+                  {
+                    "type": "ValueError",
+                    "value": "foo is not bar",
+                    "stacktrace": [
+                      {
+                        "line": 77,
+                        "filename":
+                        "django/core/handlers/base.py",
+                        "function": "wrapped_view",
+                        "context": [
+                          {"line": 74, "text": "# We could just do view_func.csrf_exempt = True, but decorators"},
+                          {"line": 75, "text": "# are nicer if they don't have side-effects, so we return a new"},
+                          {"line": 76, "text": "# function."},
+                          {"line": 77, "text": "def wrapped_view(*args, **kwargs):"},
+                          {"line": 78, "text": "  return view_func(*args, **kwargs)"},
+                          {"line": 79, "text": "wrapped_view.csrf_exempt = True"},
+                          {"line": 80, "text": ""}
+                        ]
+                      },
+                      {
+                        "line": 54,
+                        "filename":
+                        "django/core/handlers/base.py",
+                        "function": "wrapped_view",
+                        "context": [
+                          {"line": 51, "text": "# We could just do view_func.csrf_exempt = True, but decorators"},
+                          {"line": 52, "text": "# are nicer if they don't have side-effects, so we return a new"},
+                          {"line": 53, "text": "# function."},
+                          {"line": 54, "text": "def wrapped_view(*args, **kwargs):"},
+                          {"line": 55, "text": "  return view_func(*args, **kwargs)"},
+                          {"line": 56, "text": "wrapped_view.csrf_exempt = True"},
+                          {"line": 57, "text": ""}
+                        ]
+                      },
+                      {
+                        "line": 32,
+                        "filename":
+                        "django/core/handlers/base.py",
+                        "function": "wrapped_view",
+                        "context": [
+                          {"line": 29, "text": "# We could just do view_func.csrf_exempt = True, but decorators"},
+                          {"line": 30, "text": "# are nicer if they don't have side-effects, so we return a new"},
+                          {"line": 31, "text": "# function."},
+                          {"line": 32, "text": "def wrapped_view(*args, **kwargs):"},
+                          {"line": 33, "text": "  return view_func(*args, **kwargs)"},
+                          {"line": 34, "text": "wrapped_view.csrf_exempt = True"},
+                          {"line": 35, "text": ""}
+                        ]
+                      }
+                    ]
+                  }
+                ]
+              }
+            ],
             historicalData: [
               {
                 "count": randomNumber(1, 10000),
